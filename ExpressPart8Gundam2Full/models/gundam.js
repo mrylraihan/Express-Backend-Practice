3beb7fbdbae2e8f9c0a38eafcd885fead9691d05
mrylraihan <mr.ylraihan@gmail.com>
2022-06-03 13:16:00 -0700

    jst incase

--- ExpressPart8Gundam2Full/models/gundam.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const gundamSchema = new mongoose.Schema({
-    name: String
-},
-    { timestamps: true }
-)
-
-const Gundam = mongoose.model("Mech", gundamSchema)
-module.exports = Gundam
